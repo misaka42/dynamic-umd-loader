eecc7987f43c8b13ea396722f6f4810284e1f361
乔杨 <peiqiao.ppq@alipay.com>
2021-12-10 14:41:45 +0800

    feat: loadUMDFromURL / loadScript

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+const scriptDownloadingTask: Record<string, Promise<void>> = {};
+
+const isScriptExist = (url: string): boolean => {
+  return !!document.querySelector(`script[src="${url}"]`);
+};
+
+/**
+ * Load JavaSript script from online URL
+ * @param url URL of online resource
+ * @returns {void}
+ */
+export const loadScript = (url: string): Promise<void> => {
+  if (url in scriptDownloadingTask) {
+    return scriptDownloadingTask[url];
+  }
+  if (isScriptExist(url)) {
+    return Promise.resolve();
+  }
+  const p: Promise<void> = new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.onload = () => resolve();
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+
+  scriptDownloadingTask[url] = p;
+
+  return p;
+};
+
+/**
+ * Load JavaScript UMD Module From Online URL
+ * @param url URL of online resource
+ * @param name UMD Variable name of JavaScript Module
+ * @returns UMD
+ */
+export const loadUMDFromURL = <T>(url: string, name: string): Promise<T> => {
+  if (name in window) {
+    return Promise.resolve((window as any)[name] as T);
+  }
+  return loadScript(url).then(() => (window as any)[name] as T);
+};
